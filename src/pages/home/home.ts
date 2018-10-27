0302b3cbb0f30854838f9f4253f6113771bad17c
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-10-27 13:45:23 +0100

    basic list with mock data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+
+  yellow : String;
+  green : String;
+  red : String;
+  vessat: {
+    ID: number;
+    name: String;
+    status: number;
+    
+  }[];
 
   items = ["lol", "troll", "patrol"];
   constructor(public navCtrl: NavController) {
 
-  }
+    this.yellow = "#ffd62a";
+    this.green = "#2ec95c";
+    this.red = "#ff2f2f";
 
+    this.vessat = [
+      { ID: 1, name: "toilet wing B", status: 40 },
+      { ID: 2, name: "toilet wing A", status: 0 },
+      { ID: 3, name: "toilet wing C", status: 10 },
+      { ID: 4, name: "toilet wing D", status: 5 }
+    ];
+  }
 }
